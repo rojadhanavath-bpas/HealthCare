fbc4a940f113420c933755be5b77dc8ec62c7fa2
nirmalelan-bpas <34243839+nirmalelan-bpas@users.noreply.github.com>
2018-06-14 08:24:39 -0500

    Quality and Quality Benchmarks

--- MIPS/Controllers/HomeController.cs
+++ MIPS/Controllers/HomeController.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.IO;
+using ExcelDataReader;
+
 
 namespace MIPS.Controllers
 {
@@ -26,5 +32,71 @@ namespace MIPS.Controllers
 
             return View();
         }
+
+               
+        public ActionResult Quality()
+        {
+
+            string path = Server.MapPath("../Excel Files/2018-Measure-List-EditedVersion.xlsx");
+            FileStream test = new FileStream(path, FileMode.Open, FileAccess.Read);           
+            Stream stream = test;            
+            IExcelDataReader reader = null;
+            reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                      
+            DataSet result = reader.AsDataSet(new ExcelDataSetConfiguration()
+            {
+                ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+                {
+                    UseHeaderRow = true,
+                    ReadHeaderRow = (rowReader) =>
+                    {
+                        rowReader.Read();
+                    }
+
+
+                }
+            });
+            reader.Close();
+            
+            return View(result.Tables[0]);
+
+
+        }
+
+
+        public ActionResult QualityBenchMarks()
+        {
+
+            string path = Server.MapPath("../Excel Files/2018-Measure-List-EditedVersion.xlsx");
+            FileStream test = new FileStream(path, FileMode.Open, FileAccess.Read);
+            Stream stream = test;
+            IExcelDataReader reader = null;
+            reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+
+            DataSet result = reader.AsDataSet(new ExcelDataSetConfiguration()
+            {
+                ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+                {
+                    UseHeaderRow = true,
+                    ReadHeaderRow = (rowReader) =>
+                    {
+                        rowReader.Read();
+                    }
+
+
+                }
+            });
+            reader.Close();
+
+            return View(result.Tables[0]);
+
+
+        }
+
     }
+
 }
+
+
+
+        
