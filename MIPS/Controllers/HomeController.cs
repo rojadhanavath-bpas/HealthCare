81244701c2e5c30ce8aa078430b8ac370cfedcec
Roja Dhanavath <roja.dhanavath@bpaservices.com>
2018-06-18 15:35:51 -0400

    "Working on Improvement page"

--- MIPS/Controllers/HomeController.cs
+++ MIPS/Controllers/HomeController.cs
@@ -17,21 +17,7 @@ namespace MIPS.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-               
+                      
         public ActionResult Quality()
         {
 
