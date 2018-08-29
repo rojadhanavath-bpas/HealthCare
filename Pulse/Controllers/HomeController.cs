b10cac819cdc3b366b77248d16faab74c09bf094
nirmalelan-bpas <34243839+nirmalelan-bpas@users.noreply.github.com>
2018-08-29 11:04:40 -0500

    TCG - Workdriver & PULSE - Email

--- Pulse/Controllers/HomeController.cs
+++ Pulse/Controllers/HomeController.cs
@@ -357,6 +357,42 @@ namespace HealthcareAnalytics.Controllers
             return View(login);
         }
 
+        public ActionResult Email()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Email(EmailModel model)
+        {
+            using (MailMessage mm = new MailMessage(model.Email, model.To))
+            {
+                mm.Subject = model.Subject;
+                mm.Body = model.Body;
+                if (model.Attachment.ContentLength > 0)
+                {
+                    string fileName = Path.GetFileName(model.Attachment.FileName);
+                    mm.Attachments.Add(new Attachment(model.Attachment.InputStream, fileName));
+                }
+                mm.IsBodyHtml = false;
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.EnableSsl = true;
+                    NetworkCredential NetworkCred = new NetworkCredential(model.Email, model.Password);
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = NetworkCred;
+                    smtp.Port = 587;
+                    smtp.Send(mm);
+                    ViewBag.Message = "Email sent.";
+                }
+            }
+
+            return View();
+        }
+
+
+
 
     }
 
